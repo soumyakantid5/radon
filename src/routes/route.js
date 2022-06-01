6bdd26f1bb4c11bdf4393e734f2fde2a5efcadf9
soumya <soumyakantid5@gmail.com>
2022-06-01 22:41:12 +0530

    committed 6th time

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,21 +1,26 @@
 const express = require('express');
-const logger_module = require('../logger/logger')
-var date_module=require('../util/helper')
-let validator_module=require('../validator/formatter')
+const logger_module = require('../logger/logger.js')
+var date_module=require('../util/helper.js')
+let validator_module=require('../validator/formatter.js')
 
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
+router.get('/welcome', function (req, res) {
     logger_module.welcome()
+    res.send('My Welcome api!')
+});
+router.get('/date', function (req, res) {
     date_module.printDate()
     date_module.prinMonth()
     date_module.getBatchInfo();
+    res.send('My Date api!')
+});
+router.get('/validator', function (req, res) {
     validator_module.trim("    functionup spaces trimmed")
     validator_module.changetoUpperCase('this is CONVERTED to upper')
     validator_module.changetoLowerCase('THIS IS converted LOWER')
-    res.send('My first ever api!')
+    res.send('My Validator api!')
 });
-
 
 module.exports = router;
 // adding this comment for no reason
