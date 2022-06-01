712c109945168e177d8e107dfe48cf3d25d7c839
soumya <soumyakantid5@gmail.com>
2022-06-01 20:55:46 +0530

    committed 4th time

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const logger_module = require('../logger/logger')
-
+const date_module=require('../util/helper')
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
     logger_module.welcome()
+    date_module.printDate()
+    date_module.prinMonth()
+    date_module.getBatchInfo();
     res.send('My first ever api!')
 });
 
