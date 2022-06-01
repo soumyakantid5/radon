054c0864ee0988b59a433bee29e1041360ea39fa
soumya <soumyakantid5@gmail.com>
2022-06-01 21:57:33 +0530

    committed 5th time

---
+++ src/validator/formatter.js
@@ -0,0 +1,13 @@
+const trim=function(obj){
+    console.log(obj.trim())
+}
+const changetoLowerCase=function(obj){
+    console.log(obj.toLowerCase())
+}
+const changetoUpperCase=function(obj){
+    console.log(obj.toUpperCase())
+}
+
+module.exports.trim=trim
+module.exports.changetoLowerCase=changetoLowerCase
+module.exports.changetoUpperCase=changetoUpperCase
