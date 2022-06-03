322733ef45659e1728f4b2cb66096420f4d8b0d8
soumya <soumyakantid5@gmail.com>
2022-06-03 13:56:06 +0530

    updates to be done

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,45 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/students/:name', function(req, res) {
-    let studentName = req.params.name
-    console.log(studentName)
-    res.send(studentName)
-})
-
-router.get("/random" , function(req, res) {
-    res.send("hi there")
-})
-
-
-router.get("/test-api" , function(req, res) {
-    res.send("hi FunctionUp")
-})
-
-
-router.get("/test-api-2" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API")
-})
-
-
-router.get("/test-api-3" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's ")
-})
-
-
-router.get("/test-api-4" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-
-
-router.get("/test-api-5" , function(req, res) {
-    res.send("hi FunctionUp5. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-router.get("/test-api-6" , function(req, res) {
-    res.send({a:56, b: 45})
-})
+/*
 
 router.post("/test-post", function(req, res) {
     res.send([ 23, 45 , 6])
@@ -69,5 +31,56 @@ router.post("/test-post-4", function(req, res) {
     arr.push(ele)
     res.send(  { msg: arr , status: true }  )
 })
+*/
+
+let players =
+[
+{
+"name": "manish",
+"dob": "1/1/1995",
+"gender": "male",
+"city": "jalandhar",
+"sports": [
+"swimming"
+]
+},
+{
+"name": "gopal",
+"dob": "1/09/1995",
+"gender": "male",
+"city": "delhi",
+"sports": [
+"soccer"
+]
+},
+{
+"name": "lokesh",
+"dob": "1/1/1990",
+"gender": "male",
+"city": "mumbai",
+"sports": [
+"soccer"
+]
+},
+]
+
+router.post('/players',function(req,res){
+
+for(let i=0;i<players.length;i++){
+    if(players[i]["name"]!==req.body.name)
+    {
+        players.push(req.body)
+        console.log("updated")
+        break
+    }
+    else
+    console.log("Duplicate")
+   
+}
+console.log(players)
+
+
+res.send("done")
+});
 
 module.exports = router;
