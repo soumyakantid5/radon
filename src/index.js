e6bd03d708218100152319c3b71e7779a8d100ec
Soumya <soumyakantid5@gmail.com>
2022-06-11 08:22:51 +0530

    completed assignment

--- src/index.js
+++ src/index.js
@@ -2,27 +2,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
+const moment=require('moment')
 const app = express();
-
+let today=moment()
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+const mid1=function (req, res, next) {
+  console.log(today.format('YYYY-MM-DD  HH:mm:ss '))
+  console.log(req.ip)
+  console.log(req.path)
+  next();}
 
-
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
-    useNewUrlParser: true
-})
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
-
-app.use (
-    function (req, res, next) {
-        console.log ("inside GLOBAL MW");
-        next();
-  }
-  );
+app.use (mid1);
 
 app.use('/', route);
-
 
 app.listen(process.env.PORT || 3000, function () {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
